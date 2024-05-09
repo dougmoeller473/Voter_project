d7a62127f89d934522ac1e19768d0fc500f49ab3
Doug Moeller <douglasmoeller@yahoo.com>
2024-05-09 10:08:41 -0500

    Voter_Project

---
+++ election.py
@@ -0,0 +1,77 @@
+import csv
+class Election:
+
+    def __init__(self) -> None:
+        """
+        Initializes the Election class
+        """
+        self.__voters = {}
+        self.__votes = []
+        self.__voted = []
+        with open('Voting_Results.csv', 'w', newline='') as csvfile:
+            content = csv.writer(csvfile)
+            content.writerow(['Voter', 'Vote'])
+        with open('Voters.csv', 'r') as csvfile:
+            next(csvfile)
+            content = csv.reader(csvfile, delimiter=',')
+            for row in content:
+                self.__voters[row[0].lower().strip()] = int(row[1].strip())
+
+    def verify_voter(self, username, pin) -> str:
+        """
+        Method verifies if a username and pin is valid, if a username already voted, or if a username/pin is invalid
+        :param username: Voter's username
+        :param pin: Voter's pin
+        :return: "valid" if the username/pin is valid, "already voted" if voter has voted, or "not valid" if the
+        username/pin is invalid
+        """
+        if username in self.__voters and self.__voters[username] == pin:
+            return 'valid'
+        elif username in self.__voted:
+            return 'already voted'
+        else:
+            return 'not valid'
+
+    def add_vote(self, username, vote) -> None:
+        """
+        Method adds a vote for the selected box and records vote into a csv file
+        :param username: voter's username
+        :param vote: voter's vote
+        """
+        self.__votes.append(vote)
+        self.__voted.append(username)
+        with open('Voting_Results.csv', 'a', newline='') as csvfile:
+            content = csv.writer(csvfile)
+            content.writerow([username, vote])
+        self.__voters.pop(username, None)
+
+    def voting_results(self) -> str:
+        """
+        Method tallies the votes and returns a string declaring the winner(s)
+        :return: string declaring the winner(s)
+        """
+        winner = []
+        winning_score = 0
+        tallied_votes = {x: self.__votes.count(x) for x in self.__votes}
+        for key in tallied_votes:
+            if tallied_votes[key] >= winning_score:
+                winning_score = tallied_votes[key]
+                winner.append(key)
+
+        if len(winner) == 0:
+            return 'No votes were received'
+        if len(winner) == 1:
+            return f'Your winner is {winner[0]} with {winning_score} votes'
+        elif len(winner) == 2:
+            return f'Your winners are {winner[0]} and {winner[1]}\n with {winning_score} votes each'
+        else:
+            winner_string = 'Your winners are\n'
+            for i in range(len(winner)):
+                if i <= len(winner) - 3:
+                    winner_string += winner[i] + ', '
+                elif i == len(winner) - 2:
+                    winner_string += winner[i] + ', and '
+                else:
+                    winner_string += winner[i] + '\n'
+            winner_string += 'with ' + str(winning_score) + ' votes each'
+            return winner_string
---
+++ logic.py
@@ -0,0 +1,68 @@
+from PyQt6.QtWidgets import *
+from gui import *
+from election import *
+
+class Logic(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        """
+        Initializes the Logic class
+        """
+        super().__init__()
+        self.setupUi(self)
+        self.election = Election()
+        self.Clear_button.clicked.connect(lambda: self.clear())
+        self.Submit_button.clicked.connect(lambda: self.submit())
+        self.Tally_button.clicked.connect(lambda: self.tally())
+
+    def submit(self):
+        """
+        Method verifies votes and adds votes to the total when the submit button is clicked
+        """
+        self.label_feedback.setText('')
+        try:
+            username = self.User_name.text().strip().lower()
+            pin = int(self.PIN.text().strip())
+
+            if self.election.verify_voter(username, pin) == 'valid':
+                if self.radioButton_zelda.isChecked():
+                    self.election.add_vote(username,'Zelda')
+                elif self.radioButton_mario_kart.isChecked():
+                    self.election.add_vote(username, 'Mario Kart')
+                elif self.radioButton_animal_crossing.isChecked():
+                    self.election.add_vote(username, 'Animal-Crossing')
+                elif self.radioButton_smash_bros.isChecked():
+                    self.election.add_vote(username, 'Smash Bros.')
+                else:
+                    self.election.add_vote(username, self.write_in_field.text().strip().lower())
+            elif self.election.verify_voter(username, pin) == 'already voted':
+                self.label_feedback.setText('You have already voted\nPlease allow someone else to vote')
+            else:
+                self.label_feedback.setText('INVALID USERNAME AND/OR PIN')
+        except ValueError:
+            self.label_feedback.setText('Your PIN is a 4 digit number')
+
+        self.User_name.clear()
+        self.PIN.clear()
+        self.write_in_field.clear()
+        self.radioButton_zelda.setChecked(True)
+
+
+
+
+    def clear(self):
+        """
+        Method clears the fields when the clear button is clicked
+        """
+        self.User_name.clear()
+        self.PIN.clear()
+        self.write_in_field.clear()
+        self.radioButton_zelda.setChecked(True)
+        self.label_feedback.setText('')
+
+    def tally(self):
+        """
+        Method tabulates the votes when the tally button is pushed
+        """
+        self.label_feedback.setText(self.election.voting_results())
+
